251770ce531a3f7e23444d5de0b0c9bfa60cf23f
javierc22 <javier.ec.22@gmail.com>
2019-12-25 17:38:10 -0300

    Ciclo de vida de Vue JS

--- 04.js
+++ 04.js
@@ -1,6 +1,42 @@
 const app = new Vue ({
    el: '#app',
    data: {
-
+      saludo: 'Soy un ciclo de vida de Vue JS'
+   },
+   beforeCreate() {
+      console.log('beforeCreated');
+   },
+   created() {
+      // Al crear los métodos, observadores y eventos, pero aún no accede al DOM.
+      // Aún no se puede acceder a 'el'
+      console.log('created');
+   },
+   beforeMount() {
+      // Se ejecuta antes de insertar el DOM
+      console.log('beforeMount');
+   },
+   mounted() {
+      console.log('mounted');
+   },
+   beforeUpdate() {
+      // Al detectar un cambio
+      console.log('beforeUpdate');
+   },
+   updated() {
+      // Al realizar los cambios
+      console.log('updated');
+   },
+   beforeDestroy() {
+      // Antes de destruir la instancia
+      console.log('beforeDestroy');
+   },
+   destroyed() {
+      // Se destruye toda la instancia
+      console.log('destroyed');
+   },
+   methods: {
+      destruir(){
+         this.$destroy();
+      }
    }
 });
---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Vue JS - Ejemplo 4
+
+Ciclo de vida VUE JS
+
+<img src="img-app-04.jpg" width="500">
+
+<img src="lifecycle.png" width="500">
